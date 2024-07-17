f4ec75316d5f9d0fa28e5af4d4f72fda0a824095
ChrizDope <incubusid@googlemail.com>
2024-07-17 14:08:17 +0200

    Added controller- and service-tests

---
+++ backend/src/test/java/org/west/backend/controller/GameControllerTest.java
@@ -0,0 +1,52 @@
+package org.west.backend.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.west.backend.model.Game;
+import org.west.backend.repository.GameRepository;
+
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class GameControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private GameRepository mockGameRepository;
+
+    private List<Game> gameTestData;
+
+    @BeforeEach
+    void setUp(){
+        gameTestData = List.of(
+                new Game("1", "The Legend of Zelda: Breath of the Wild", "2017-03-03", false)
+        );
+    }
+
+    @Test
+    void getAllGames() throws Exception {
+        when(mockGameRepository.findAll()).thenReturn(gameTestData);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json("""
+                                                                [{
+                                                                    name: "The Legend of Zelda: Breath of the Wild",
+                                                                    releaseDate: "2017-03-03",
+                                                                    fav: false
+                                                                }]
+                                                                """));
+    }
+}
---
+++ backend/src/test/java/org/west/backend/service/GameServiceTest.java
@@ -0,0 +1,57 @@
+package org.west.backend.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.west.backend.model.Game;
+import org.west.backend.repository.GameRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class GameServiceTest {
+
+    private final GameRepository mockGameRepository = mock(GameRepository.class);
+    private final GameService gameService = new GameService(mockGameRepository);
+    private List<Game> testData;
+
+
+    @BeforeEach
+    void setUp(){
+        testData = List.of(
+                new Game("1", "The Legend of Zelda: Breath of the Wild", "2017-03-03", false),
+                new Game("2", "God of War", "2018-04-20", false),
+                new Game("3", "Red Dead Redemption 2", "2018-10-26", false)
+        );
+    }
+
+    @Test
+    void getAllGames_shouldReturnListOfAllGames_whenCalled() {
+        // GIVEN
+        List<Game> expected = testData;
+        when(mockGameRepository.findAll()).thenReturn(expected);
+
+        // WHEN
+        List<Game> actual = mockGameRepository.findAll();
+
+        // THEN
+        assertEquals(expected,actual);
+        verify(mockGameRepository).findAll();
+    }
+
+    @Test
+    void getAllGames_shouldReturnEmptyList_whenCalled() {
+        // GIVEN
+        List<Game> expected = new ArrayList<>();
+        when(mockGameRepository.findAll()).thenReturn(expected);
+
+        // WHEN
+        List<Game> actual = mockGameRepository.findAll();
+
+        // THEN
+        assertEquals(expected,actual);
+        verify(mockGameRepository).findAll();
+    }
+}
