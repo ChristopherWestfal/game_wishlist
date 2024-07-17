cad6460453c2d1b4d954f9209433fbc11d8164b7
ChrizDope <incubusid@googlemail.com>
2024-07-17 12:41:09 +0200

    Added getAllGames in backend

--- backend/src/main/java/org/west/backend/service/GameService.java
+++ backend/src/main/java/org/west/backend/service/GameService.java
@@ -2,11 +2,18 @@ package org.west.backend.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.west.backend.model.Game;
 import org.west.backend.repository.GameRepository;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 
 public class GameService {
     private final GameRepository gameRepository;
+
+    public List<Game> getAllGames() {
+        return gameRepository.findAll();
+    }
 }
