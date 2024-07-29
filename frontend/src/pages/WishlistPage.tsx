656eb74e14c3d69ad37a3fe5e7c1fb011279c79a
ChrizDope <incubusid@googlemail.com>
2024-07-29 14:46:01 +0200

    modified wishlistpage

--- frontend/src/pages/WishlistPage.tsx
+++ frontend/src/pages/WishlistPage.tsx
@@ -24,14 +24,13 @@ export default function WishlistPage(props: Readonly<WishlistPageProps>) {
             <Box sx={{ display: 'flex' }}>
                 <Sidenav/>
                 <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-                    <Typography paragraph>
+                    <Typography component="div" paragraph>
                         <Grid container spacing={1}>
                             {
-                                props.games.map((game) => <Grid item xs={3}> <GameCardWishlist
+                                props.games.map((game) => <Grid item xs={3} key={game.id}> <GameCardWishlist
                                     putGame={props.putGame}
                                     deleteById={props.deleteById}
-                                    game={game}
-                                    key={game.id}/></Grid>)
+                                    game={game}/></Grid>)
                             }
                         </Grid>
                     </Typography>
